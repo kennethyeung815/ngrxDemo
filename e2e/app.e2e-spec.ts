8c95e32179b487b68df1cc9a61020a370653bbc5
Kenneth Yeung <kennethyeung815@gmail.com>
2017-10-08 00:04:30 -0400

    - NCDevCon NgRx Demo

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('ngrx-demo App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display current count', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Current Count: 0');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { counterReducer } from './reducers/counter.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		StoreModule.forRoot({ counter: counterReducer }),
+		StoreDevtoolsModule.instrument({
+			maxAge: 100
+		}),
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
